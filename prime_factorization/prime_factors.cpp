c857b753368fd57a9e01b00836c789694daa03b1
bekor <bekor88@gmail.com>
2018-02-05 23:57:33 +0100

    solution inculded

---
+++ prime_factorization/prime_factors.cpp
@@ -0,0 +1,2 @@
+#include "stdafx.h"
+#include "prime_factors.h"
---
+++ prime_factorization/prime_factors.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <set>
+
+// TODO: Here implement the function for calculating the distinct prime factors of the number.
+namespace factors {
+	static std::set<int> getPrimes(int number) {
+		// implementation here
+	}
+}
